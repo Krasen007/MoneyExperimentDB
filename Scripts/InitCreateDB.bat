71138e2fdec1b6cfc51c19e11d61ece6887829dc
Krasen <krasen007@gmail.com>
2019-10-17 02:14:45 +0300

    Update database

--- Scripts/InitCreateDB.bat
+++ Scripts/InitCreateDB.bat
@@ -4,6 +4,6 @@ git config user.name "Krasen Ivanov"
 git config user.email "krasen007@gmail.com"
 git add .
 git commit -m "Create database"
-echo *** Creating remote database ***
 git remote add origin https://github.com/Krasen007/MoneyExperimentDB.git
 git push -u origin master --quiet
+echo *** Creating remote database complete ***
